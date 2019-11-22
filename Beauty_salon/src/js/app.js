4f82de034e247bb98fdb2fe3aca5dd26aa0e23da
Konstantin <fandeluk.k@gmai.com>
2019-11-22 22:51:29 +0200

    Учусь добавлять медиа запросы

--- Beauty_salon/src/js/app.js
+++ Beauty_salon/src/js/app.js
@@ -1,15 +1,15 @@
 let arrDataId = document.querySelectorAll("[data-id]");
-// Повесить обратчики событий на все елементы
 for (let i = 0; i < arrDataId.length; i++) {
-	arrDataId[i].addEventListener("click", function () {
-		let id = arrDataId[i].getAttribute("data-id");
-		let element = document.getElementById(id);
-		element.classList.add("active")
-	});
+  arrDataId[i].addEventListener("click", function() {
+    let id = arrDataId[i].getAttribute("data-id");
+    let element = document.getElementById(id);
+    element.classList.add("active");
+  });
 }
-// Закрыть модалку
-// modalCallback.addEventListener("click", function (ev) {
-// 	if (ev.target.closest(".modal__close") || ev.target.classList.contains("modal")) {
-// 		modalCallback.classList.remove("active")
-// 	}
-// })
+modalCallback.addEventListener("click", function(ev) {
+  if ( eval.target.closest(".modal__close") ||
+    ev.target.classList.contains("modal")
+  ) {
+    modalCallback.classList.remove("active");
+  }
+});
