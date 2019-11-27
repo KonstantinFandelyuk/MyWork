7e87b8a6e790f8cc703d8d3365503ea3eb4a3fa4
Konstantin <fandeluk.k@gmail.com>
2019-11-27 23:42:52 +0200

    add animation

--- Beauty_salon/build/js/app.js
+++ Beauty_salon/build/js/app.js
@@ -1,15 +1,38 @@
-let arrDataId = document.querySelectorAll("[data-id]");
-for (let i = 0; i < arrDataId.length; i++) {
-  arrDataId[i].addEventListener("click", function() {
-    let id = arrDataId[i].getAttribute("data-id");
-    let element = document.getElementById(id);
-    element.classList.add("active");
-  });
+var buttonElement = document.querySelector("[data-id=modal-callback]");
+var modalElement = document.querySelector(".modal");
+
+// console.log(modalButtonElement);
+
+buttonElement.addEventListener("click", function () {
+	modalElement.classList.add("active");
+})
+
+var closeElement = document.querySelector(".modal__close");
+
+closeElement.addEventListener("click", function () {
+	modalElement.classList.remove("active");
+})
+
+
+var catalog__list = document.querySelector(".catalog__list");
+
+var catalog__listPosTop = catalog__list.getBoundingClientRect().top;
+
+var scroll = function () {
+	if (window.pageYOffset >= catalog__listPosTop - 300) {
+		window.removeEventListener("scroll", scroll) // Важно
+		catalog__list.classList.add("slideInLeft")
+	}
 }
-modalCallback.addEventListener("click", function(ev) {
-  if ( eval.target.closest(".modal__close") ||
-    ev.target.classList.contains("modal")
-  ) {
-    modalCallback.classList.remove("active");
-  }
+window.addEventListener("scroll", scroll)
+
+// ----------------------Animated social icon----------------------------------------------------------
+
+var socialIcon = document.querySelector(".social-icon__item");
+
+socialIcon.addEventListener("mouseover", function () {
+	socialIcon.classList.add("flip");
+	this.addEventListener("mouseout", function () {
+		socialIcon.classList.remove("flip");
+	});
 });
