119a7cca107449fa09edbc0cd81b35b2ae340d88
Konstantin <fandeluk.k@gmail.com>
2019-12-25 23:49:35 +0200

    Паралельный проект на содание СРМ системы для ЖКХ

---
+++ Portal/build/js/app.js
@@ -0,0 +1,7 @@
+// const openMenu = document.querySelector(".dashboard-shwitch"),
+// 	leftMenu = document.querySelector(".show-menu, .animated");
+
+// openMenu.addEventListener("change", function () {
+// 	leftMenu.classList.add("slideInLeft");
+// 	leftMenu.classList.toggle("active");
+// })
