4d131d9762f5edfa69dd1d723bb1660730c037fe
Konstantin <fandeluk.k@gmail.com>
2020-01-10 12:40:23 +0200

    Обучение по js

---
+++ JS Tusk/Task - 14.12.2019/3.js
@@ -0,0 +1,10 @@
+const a = ["Ivan", "Kostya", "Kek", "Kolom"];
+
+/*
+ написать две функции которые сортируют масив по алфавиту
+ 
+ "a".charCodeAt() ==> 97 ==>  https://ru.wikipedia.org/wiki/ASCII
+ "Kolom".split("") ==> ["K", "o", "l", "o", "m"]  ==> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+ 
+
+*/
---
+++ JS Tusk/if/task/one.js
@@ -0,0 +1,21 @@
+const random = (min, max) =>
+  Math.round(min - 0.5 + Math.random() * (max - min + 1));
+
+var a = new Array(10);
+var n = random(0, 9);
+var k = random(0, 9);
+
+for (var i = 0; i < a.length; i++) a[i] = random(0, 10);
+
+console.log("a: ", a, "n: ", n, "k: ", k);
+
+/* Code here */
+
+if (i <= n || i >= k) {
+
+}
+
+
+
+
+/* end your code */
