c9d5430555c5f59c22e67c6725e106696ac38fb3
Konstantin <fandeluk.k@gmail.com>
2020-02-28 21:15:12 +0200

    First page

---
+++ Nuts/build/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".slider").slick({
+    arrows: false,
+    dots: true,
+  });
+});
+
+$(document).ready(function() {
+  $(".feedback-slider").slick({
+    arrows: false,
+    dots: true,
+  });
+});
