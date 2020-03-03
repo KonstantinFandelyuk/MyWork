6e4abacf6e55d96156927b6ff82543b59abc43f8
Konstantin <fandeluk.k@gmail.com>
2020-03-03 21:17:51 +0200

    Добавил модальное окно

--- Nuts/src/js/app.js
+++ Nuts/src/js/app.js
@@ -1,3 +1,19 @@
+const bodyElement = document.getElementById("body");
+const modalElement = document.getElementById("modal");
+const buttonElement = document.querySelector(".burger__menu");
+const closeModalElement = document.querySelector(".modal__close");
+
+buttonElement.addEventListener("click", function() {
+  bodyElement.classList.add("hidden-scroll");
+  modalElement.classList.add("active");
+  // animatedModalElement.classList.add("fadeInDown");
+});
+
+closeModalElement.addEventListener("click", function() {
+  modalElement.classList.remove("active");
+  bodyElement.classList.remove("hidden-scroll");
+});
+
 $(document).ready(function() {
   $(".feedback-clients").slick({
     arrows: false,
