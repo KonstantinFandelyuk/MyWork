9437c79f843369d9238b2398b4e075fc87db7231
Konstantin <fandeluk.k@gmail.com>
2020-03-07 22:23:05 +0200

    Старт Отель для кошек

--- HotelCat/src/js/app.js
+++ HotelCat/src/js/app.js
@@ -2,5 +2,16 @@ $(document).ready(function() {
   $(".slider").slick({
     arrows: false,
     dots: true,
+    autoplay: true,
+  });
+});
+
+$(document).ready(function() {
+  $(".slider-feedback").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
   });
 });
