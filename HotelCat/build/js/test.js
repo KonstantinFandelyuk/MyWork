beef21bdd35dc71df095721a86aa6abd5fd45d8d
Konstantin <fandeluk.k@gmail.com>
2020-04-03 00:34:24 +0300

    fix bug

--- HotelCat/build/js/test.js
+++ HotelCat/build/js/test.js
@@ -1,42 +1,42 @@
-// ------CHECBOX --------
-let allCheckbox = Array.from(document.querySelectorAll("input[type=checkbox]"));
+// // ------CHECBOX --------
+// let allCheckbox = Array.from(document.querySelectorAll("input[type=checkbox]"));
 
-let squareInput = Array.from(document.querySelectorAll("input[name=square-input]"));
+// let squareInput = Array.from(document.querySelectorAll("input[name=square-input]"));
 
-//-----------------------------
-// ------- Button ---------
-let buttonFilterSumbit = document.querySelector(".button__filter--sumbit");
-let buttonFilterReset = document.querySelector(".button__filter--reset");
-// -------------------------
+// //-----------------------------
+// // ------- Button ---------
+// let buttonFilterSumbit = document.querySelector(".button__filter--sumbit");
+// let buttonFilterReset = document.querySelector(".button__filter--reset");
+// // -------------------------
 
-let catalogItemProduct = Array.from(document.querySelectorAll(".catalog__list-item"));
-let catalogText = Array.from(document.querySelectorAll(".catalog-text "));
+// let catalogItemProduct = Array.from(document.querySelectorAll(".catalog__list-item"));
+// let catalogText = Array.from(document.querySelectorAll(".catalog-text "));
 
-// let squareListItem = Array.from(document.querySelectorAll(".square"));
-let res = []; // - Массив данных  выбраных чекбоксов
+// // let squareListItem = Array.from(document.querySelectorAll(".square"));
+// let res = []; // - Массив данных  выбраных чекбоксов
 
-// --- Проходит по всем чекбоксам и активирует кнопку ---------
+// // --- Проходит по всем чекбоксам и активирует кнопку ---------
 
-// allCheckbox.map((value) => {
-//   value.onclick = function() {
-//     if (value) {
-//       buttonFilterSumbit.classList.add("active");
-//     }
-//   };
+// // allCheckbox.map((value) => {
+// //   value.onclick = function() {
+// //     if (value) {
+// //       buttonFilterSumbit.classList.add("active");
+// //     }
+// //   };
+// // });
+
+// // // --------------------------------------------
+
+// allCheckbox.forEach((item, index) => {
+//   item.addEventListener("change", (e) => {
+//     catalogText.forEach((value, i) => {
+//       let catalogTextAtr = value.getAttributeNames()[1];
+//       let inputAtr = item.getAttributeNames()[1];
+//       console.log(inputAtr);
+//       // if (inputAtr === catalogTextAtr && catalogTextAtr[i] == i) {
+//       //   console.log("res");
+//       //   // res.push(element);
+//       // }
+//     });
+//   });
 // });
-
-// // --------------------------------------------
-
-allCheckbox.forEach((item, index) => {
-  item.addEventListener("change", (e) => {
-    catalogText.forEach((value, i) => {
-      let catalogTextAtr = value.getAttributeNames()[1];
-      let inputAtr = item.getAttributeNames()[1];
-      console.log(inputAtr);
-      // if (inputAtr === catalogTextAtr && catalogTextAtr[i] == i) {
-      //   console.log("res");
-      //   // res.push(element);
-      // }
-    });
-  });
-});
